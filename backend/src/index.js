0d591443f9d20e6d1432cc367fe33f721576d0fc
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 02:40:04 -0300

    Implementação das Rotas e Cadastros.
    Inicialização do arquivo de Rotas da Aplicação (routes.js) e importação no Index(index.js);

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
     const app = express();
 
         mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack10', {
@@ -7,9 +8,6 @@ const mongoose = require('mongoose');
             useUnifiedTopology: true,
         });
 
-            app.get('/', (request, response) => {
-                return response.json({ message: "Hello! =)" });
-            });
-
-                app.use(express.json);
-                app.listen(3333);
+            app.use(express.json);
+            app.use(routes);
+            app.listen(3333);
---
+++ backend/src/routes.js
@@ -0,0 +1,8 @@
+const { Router } = require('express');
+    const routes = Router();
+    
+        routes.get('/', (request, response) => {
+            return response.json({ message: "Hello! =)" });
+        });
+
+            module.exports = routes;
