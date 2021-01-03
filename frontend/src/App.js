9d1b8913dfcd89261555eb768e9a17a33e118cc0
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 16:17:06 -0300

    Implementação inicial do FRONTEND.
    Implementado inicialmente o FRONTEND da aplicação com os elementos e estilos iniciais.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
+import React from 'react';
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong> Cadastrar </strong>
+          <form>
+            <div className="input-block">
+              <label htmlFor="github_username"> Usuário do GitHub </label>
+                <input name="github_username" id="github_username"/>
+            </div>
+            <div className="input-block">
+              <label htmlFor="techs"> Tecnologias </label>
+                <input name="techs" id="techs" required/>
+            </div>
+              <div className="input-group">
+                <div className="input-block">
+                  <label htmlFor="latitude"> Latitude </label>
+                    <input name="latitude" id="latitude" required/>
+                </div>
+                <div className="input-block">
+                  <label htmlFor="longitude"> Longitude </label>
+                  <input name="longitude" id="longitude" required/>
+                </div>
+              </div>
+                <button type="submit"> Salvar </button>
+          </form>
+      </aside>
+        <main>
+          <ul>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                  <div className="user-info">
+                    <strong> Alisson Prates Peres </strong>
+                      <span> NodeJS, ReactJS, React Native </span>
+                  </div>
+              </header>
+                <p> code the world </p>
+                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                  <div className="user-info">
+                    <strong> Alisson Prates Peres </strong>
+                      <span> NodeJS, ReactJS, React Native </span>
+                  </div>
+              </header>
+                <p> code the world </p>
+                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                  <div className="user-info">
+                    <strong> Alisson Prates Peres </strong>
+                      <span> NodeJS, ReactJS, React Native </span>
+                  </div>
+              </header>
+                <p> code the world </p>
+                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+            </li>
+            <li className="dev-item">
+              <header>
+                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                  <div className="user-info">
+                    <strong> Alisson Prates Peres </strong>
+                      <span> NodeJS, ReactJS, React Native </span>
+                  </div>
+              </header>
+                <p> code the world </p>
+                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+            </li>
+          </ul>
+        </main>
     </div>
   );
 }
