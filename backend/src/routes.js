8fb4db39f23cc18d1fbb26a955eac713d7b4fea9
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 03:40:20 -0300

    Criação e Configuração do PointSchema para o Mapa.
    Criado o Schema para o cadastro dos Devs com a Localização no "PointSchema.js" e configurado nos arquivos "Dev.js" e "routes.js" - o PointSchema servirá para exibir a localização do usuário no Mapa do Aplicativo.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,12 +2,14 @@ const { Router } = require('express');
 const axios = require('axios');
 const Dev = require('./models/Dev');
 const routes = Router();
+
     routes.post('/devs', async (request, response) => {
-        const { github_username, techs } = request.body;
+        const { github_username, techs, latitude, longitude } = request.body;
         const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
         const { name = login, bio, avatar_url } = apiResponse.data;
         const techsArray = techs.split(',').map(tech => tech.trim());
-        const dev = await Dev.create({ name, github_username, bio, avatar_url, techs: techsArray });
+        const location = { type: 'Point', coordinates: [ longitude, latitude ] }
+        const dev = await Dev.create({ name, github_username, bio, avatar_url, techs: techsArray, location });
         return response.json(dev);
     });
 
