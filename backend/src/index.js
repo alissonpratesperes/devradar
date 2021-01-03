0da92825886e8e34bb4b30f4a47275f31e5031a4
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 03:24:36 -0300

    Instalação do Axios com o GitHub e implementação inicial do CRUD.
    Corrigida a configuração do JSON para o Express que estava incorreta;
    Instalação do Axios para a comunicação entre APIs e configuração nos arquivos para a comunicação com a API do GitHub;
    Implementação inicial do CRUD da aplicação.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
-    const app = express();
+const app = express();
+        
+    mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack10', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
 
-        mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack10', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-
-            app.use(express.json);
-            app.use(routes);
-            app.listen(3333);
+        app.use(express.json());
+        app.use(routes);
+        app.listen(3333);
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
-    const routes = Router();
-    
-        routes.get('/', (request, response) => {
-            return response.json({ message: "Hello! =)" });
-        });
+const axios = require('axios');
+const Dev = require('./models/Dev');
+const routes = Router();
+    routes.post('/devs', async (request, response) => {
+        const { github_username, techs } = request.body;
+        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+        const { name = login, bio, avatar_url } = apiResponse.data;
+        const techsArray = techs.split(',').map(tech => tech.trim());
+        const dev = await Dev.create({ name, github_username, bio, avatar_url, techs: techsArray });
+        return response.json(dev);
+    });
 
-            module.exports = routes;
+        module.exports = routes;
