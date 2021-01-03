69b94818df195a184db6368b2a122334b20e4a74
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 17:25:15 -0300

    ConsoleLog das Coordenadas de Localização.
    Testes iniciais no FrontEnd para verificar se a Coleta das Coordenadas (Latitude e Longitude) estão funcionando a partir do JS (navigator.geolocation.getCurrentPosition);
    Sucesso da funcionalidade na Console do Chrome Dev Tools após permissão dada no System Preferences > Security & Privacy > Location > Google Chrome do Mac.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,86 +1,100 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
-  return (
-    <div id="app">
-      <aside>
-        <div className="brand"> </div>
-          <form>
-            <div className="input-block">
-              <label htmlFor="github_username"> Usuário do GitHub </label>
-                <input name="github_username" id="github_username"/>
-            </div>
-            <div className="input-block">
-              <label htmlFor="techs"> Tecnologias </label>
-                <input name="techs" id="techs" required/>
-            </div>
-              <div className="input-group">
-                <div className="input-block">
-                  <label htmlFor="latitude"> Latitude </label>
-                    <input name="latitude" id="latitude" required/>
-                </div>
-                <div className="input-block">
-                  <label htmlFor="longitude"> Longitude </label>
-                  <input name="longitude" id="longitude" required/>
-                </div>
+  useEffect( () => {
+    navigator.geolocation.getCurrentPosition( 
+      (position) => {
+        console.log(position);
+      },
+      (err) => {
+        console.log(err);
+      },
+        {
+          timeout: 30000,
+        }
+    )
+  }, [] );
+
+    return (
+      <div id="app">
+        <aside>
+          <div className="brand"> </div>
+            <form>
+              <div className="input-block">
+                <label htmlFor="github_username"> Usuário do GitHub </label>
+                  <input name="github_username" id="github_username"/>
               </div>
-                <button type="submit"> Salvar </button>
-          </form>
-      </aside>
-        <main>
-          <ul>
-            <li className="dev-item">
-              <header>
-                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                  <div className="user-info">
-                    <strong> Alisson Prates Peres </strong>
-                      <span> NodeJS, ReactJS, React Native </span>
+              <div className="input-block">
+                <label htmlFor="techs"> Tecnologias </label>
+                  <input name="techs" id="techs" required/>
+              </div>
+                <div className="input-group">
+                  <div className="input-block">
+                    <label htmlFor="latitude"> Latitude </label>
+                      <input name="latitude" id="latitude" required/>
                   </div>
-              </header>
-                <p> code the world </p>
-                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-            </li>
-            <li className="dev-item">
-              <header>
-                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                  <div className="user-info">
-                    <strong> Alisson Prates Peres </strong>
-                      <span> NodeJS, ReactJS, React Native </span>
+                  <div className="input-block">
+                    <label htmlFor="longitude"> Longitude </label>
+                    <input name="longitude" id="longitude" required/>
                   </div>
-              </header>
-                <p> code the world </p>
-                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-            </li>
-            <li className="dev-item">
-              <header>
-                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                  <div className="user-info">
-                    <strong> Alisson Prates Peres </strong>
-                      <span> NodeJS, ReactJS, React Native </span>
-                  </div>
-              </header>
-                <p> code the world </p>
-                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-            </li>
-            <li className="dev-item">
-              <header>
-                <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                  <div className="user-info">
-                    <strong> Alisson Prates Peres </strong>
-                      <span> NodeJS, ReactJS, React Native </span>
-                  </div>
-              </header>
-                <p> code the world </p>
-                  <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-            </li>
-          </ul>
-        </main>
-    </div>
-  );
+                </div>
+                  <button type="submit"> Salvar </button>
+            </form>
+        </aside>
+          <main>
+            <ul>
+              <li className="dev-item">
+                <header>
+                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                    <div className="user-info">
+                      <strong> Alisson Prates Peres </strong>
+                        <span> NodeJS, ReactJS, React Native </span>
+                    </div>
+                </header>
+                  <p> code the world </p>
+                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+              </li>
+              <li className="dev-item">
+                <header>
+                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                    <div className="user-info">
+                      <strong> Alisson Prates Peres </strong>
+                        <span> NodeJS, ReactJS, React Native </span>
+                    </div>
+                </header>
+                  <p> code the world </p>
+                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+              </li>
+              <li className="dev-item">
+                <header>
+                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                    <div className="user-info">
+                      <strong> Alisson Prates Peres </strong>
+                        <span> NodeJS, ReactJS, React Native </span>
+                    </div>
+                </header>
+                  <p> code the world </p>
+                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+              </li>
+              <li className="dev-item">
+                <header>
+                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                    <div className="user-info">
+                      <strong> Alisson Prates Peres </strong>
+                        <span> NodeJS, ReactJS, React Native </span>
+                    </div>
+                </header>
+                  <p> code the world </p>
+                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+              </li>
+            </ul>
+          </main>
+      </div>
+    );
 }
 
-export default App;
+  export default App;
