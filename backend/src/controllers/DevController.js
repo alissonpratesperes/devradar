eae8f9376fc616111a09f9bd87c473d7abd108bc
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 03:52:27 -0300

    Finalização da Rota de Cadastro de Devs.
    Implementada Verificação de Dev Existente, para evitar Cadastros Duplicados no banco de dados.

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -4,12 +4,15 @@ const Dev = require('../models/Dev');
     module.exports = {
         async store(request, response) {
             const { github_username, techs, latitude, longitude } = request.body;
-            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
-            const { name = login, bio, avatar_url } = apiResponse.data;
-            const techsArray = techs.split(',').map(tech => tech.trim());
-            const location = { type: 'Point', coordinates: [ longitude, latitude ] }
-            const dev = await Dev.create({ name, github_username, bio, avatar_url, techs: techsArray, location });
-
-            return response.json(dev);
+                let dev = await Dev.findOne({ github_username });
+                    if(!dev) {
+                        const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+                        const { name = login, bio, avatar_url } = apiResponse.data;
+                        const techsArray = techs.split(',').map(tech => tech.trim());
+                        const location = { type: 'Point', coordinates: [ longitude, latitude ] }
+                            dev = await Dev.create({ name, github_username, bio, avatar_url, techs: techsArray, location });
+                    }
+            
+                        return response.json(dev);
         }
     };
