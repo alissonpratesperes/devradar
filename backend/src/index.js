dfb9eeebd21fbed317cd9c43793fbf9e37912d26
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 17:41:30 -0300

    Implementação dos Estados nos Inputs do Formulário no FRONTEND e do CORS no BACKEND.
    Implementação do CORS no BACKEND para o acesso do FRONTEND;
    Implementadas as Funções e Estados nos Inputs do Form de Cadastro.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 const app = express();
         
     mongoose.connect('mongodb+srv://omnistack_dev:KcAtSiNm0O@mavericks.pwlbv.mongodb.net/omnistack10', { useNewUrlParser: true, useUnifiedTopology: true });
 
+        app.use(cors());
         app.use(express.json());
         app.use(routes);
         app.listen(3333);
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,100 +1,112 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
 
 function App() {
-  useEffect( () => {
-    navigator.geolocation.getCurrentPosition( 
-      (position) => {
-        console.log(position);
-      },
-      (err) => {
-        console.log(err);
-      },
-        {
-          timeout: 30000,
-        }
-    )
-  }, [] );
+  const [ github_username, setGithubUsername ] = useState('');
+  const [ techs, setTechs ] = useState('');
+  const [ latitude, setLatitude ] = useState('');
+  const [ longitude, setLongitude] = useState('');
 
-    return (
-      <div id="app">
-        <aside>
-          <div className="brand"> </div>
-            <form>
-              <div className="input-block">
-                <label htmlFor="github_username"> Usuário do GitHub </label>
-                  <input name="github_username" id="github_username"/>
-              </div>
-              <div className="input-block">
-                <label htmlFor="techs"> Tecnologias </label>
-                  <input name="techs" id="techs" required/>
-              </div>
-                <div className="input-group">
+    useEffect( () => {
+      navigator.geolocation.getCurrentPosition( 
+        (position) => {
+          const { latitude, longitude } = position.coords;
+            setLatitude(latitude);
+            setLongitude(longitude);
+        },
+        (err) => {
+          console.log(err);
+        },
+          {
+            timeout: 30000,
+          }
+      )
+    }, [] );
+
+      async function handleAddDev(e) {
+        e.preventDefault();
+          
+      }
+
+        return (
+          <div id="app">
+            <aside>
+              <div className="brand"> </div>
+                <form onSubmit={ handleAddDev }>
                   <div className="input-block">
-                    <label htmlFor="latitude"> Latitude </label>
-                      <input name="latitude" id="latitude" required/>
+                    <label htmlFor="github_username"> Usuário do GitHub </label>
+                      <input name="github_username" id="github_username" required value={ github_username } onChange={ e => setGithubUsername(e.target.value) }/>
                   </div>
                   <div className="input-block">
-                    <label htmlFor="longitude"> Longitude </label>
-                    <input name="longitude" id="longitude" required/>
+                    <label htmlFor="techs"> Tecnologias </label>
+                      <input name="techs" id="techs" required value={ techs } onChange={ e => setTechs(e.target.value) }/>
                   </div>
-                </div>
-                  <button type="submit"> Salvar </button>
-            </form>
-        </aside>
-          <main>
-            <ul>
-              <li className="dev-item">
-                <header>
-                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                    <div className="user-info">
-                      <strong> Alisson Prates Peres </strong>
-                        <span> NodeJS, ReactJS, React Native </span>
+                    <div className="input-group">
+                      <div className="input-block">
+                        <label htmlFor="latitude"> Latitude </label>
+                          <input type="number" name="latitude" id="latitude" required value={ latitude } onChange={ e => setLatitude(e.target.value) }/>
+                      </div>
+                      <div className="input-block">
+                        <label htmlFor="longitude"> Longitude </label>
+                        <input type="number" name="longitude" id="longitude" required value={ longitude } onChange={ e => setLongitude(e.target.value) }/>
+                      </div>
                     </div>
-                </header>
-                  <p> code the world </p>
-                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-              </li>
-              <li className="dev-item">
-                <header>
-                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                    <div className="user-info">
-                      <strong> Alisson Prates Peres </strong>
-                        <span> NodeJS, ReactJS, React Native </span>
-                    </div>
-                </header>
-                  <p> code the world </p>
-                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-              </li>
-              <li className="dev-item">
-                <header>
-                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                    <div className="user-info">
-                      <strong> Alisson Prates Peres </strong>
-                        <span> NodeJS, ReactJS, React Native </span>
-                    </div>
-                </header>
-                  <p> code the world </p>
-                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-              </li>
-              <li className="dev-item">
-                <header>
-                  <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
-                    <div className="user-info">
-                      <strong> Alisson Prates Peres </strong>
-                        <span> NodeJS, ReactJS, React Native </span>
-                    </div>
-                </header>
-                  <p> code the world </p>
-                    <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
-              </li>
-            </ul>
-          </main>
-      </div>
-    );
+                      <button type="submit"> Salvar </button>
+                </form>
+            </aside>
+              <main>
+                <ul>
+                  <li className="dev-item">
+                    <header>
+                      <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                        <div className="user-info">
+                          <strong> Alisson Prates Peres </strong>
+                            <span> NodeJS, ReactJS, React Native </span>
+                        </div>
+                    </header>
+                      <p> code the world </p>
+                        <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+                  </li>
+                  <li className="dev-item">
+                    <header>
+                      <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                        <div className="user-info">
+                          <strong> Alisson Prates Peres </strong>
+                            <span> NodeJS, ReactJS, React Native </span>
+                        </div>
+                    </header>
+                      <p> code the world </p>
+                        <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+                  </li>
+                  <li className="dev-item">
+                    <header>
+                      <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                        <div className="user-info">
+                          <strong> Alisson Prates Peres </strong>
+                            <span> NodeJS, ReactJS, React Native </span>
+                        </div>
+                    </header>
+                      <p> code the world </p>
+                        <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+                  </li>
+                  <li className="dev-item">
+                    <header>
+                      <img src="https://avatars1.githubusercontent.com/u/52282116?s=460&u=4b1ca632b207c0133a3546fc41f5572f856b1065&v=4" alt="Alisson Prates Peres"/>
+                        <div className="user-info">
+                          <strong> Alisson Prates Peres </strong>
+                            <span> NodeJS, ReactJS, React Native </span>
+                        </div>
+                    </header>
+                      <p> code the world </p>
+                        <a href="https://github.com/alissonpratesperes"> Acessar Perfil no GitHub </a>
+                  </li>
+                </ul>
+              </main>
+          </div>
+        );
 }
 
   export default App;
