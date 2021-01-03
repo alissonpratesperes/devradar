041652af596618a65637ed034e8d7295e5e93436
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-03 03:57:49 -0300

    Implementada a Rota de Listagem de Devs.
    Implementada a Rota para a Listagem dos Devs.

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const DevController = require('./controllers/DevController');
 const routes = Router();
 
+    routes.get('/devs', DevController.index);
     routes.post('/devs', DevController.store);
 
         module.exports = routes;
