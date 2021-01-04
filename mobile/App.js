3fdd25cdc58254610deeb2a370cc4eb406c267e1
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-04 05:05:21 -0300

    Implementação inicial.
    Implementadas as dependências iniciais necessárias para o projeto Mobile;
    Implementadas inicialmente as Páginas e Rotas do App.

--- mobile/App.js
+++ mobile/App.js
@@ -1,21 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from './src/routes';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text> Hello OmniStack!!! </Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  export default function App() {
+    return (
+      <Routes />
+    );
+  }
