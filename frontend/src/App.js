fadd7f193247ab3e832d6cbcc858a69bc2247ee4
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-01-04 03:43:54 -0300

    Abstração e Componentização de códigos.
    Realizada a Componentização abstraindo os códigos-fonte do Formulário de Cadastro e Listagem dos Devs bem como dos CSSs também;
    Ajustada indentação e organização geral do código-fonte da aplicação.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,90 +4,40 @@ import './global.css';
 import './App.css';
 import './Sidebar.css';
 import './Main.css';
+import DevForm from './components/DevForm/Index';
+import DevItem from './components/DevItem/Index';
 
-function App() {
-  const [ devs, setDevs ] = useState([]);
-  const [ github_username, setGithubUsername ] = useState('');
-  const [ techs, setTechs ] = useState('');
-  const [ latitude, setLatitude ] = useState('');
-  const [ longitude, setLongitude] = useState('');
+  function App() {
+    const [ devs, setDevs ] = useState([]);
 
-    useEffect( () => {
-      navigator.geolocation.getCurrentPosition( 
-        (position) => {
-          const { latitude, longitude } = position.coords;
-            setLatitude(latitude);
-            setLongitude(longitude);
-        },
-        (err) => {
-          console.log(err);
-        },
-          {
-            timeout: 30000,
-          }
-      )
-    }, [] );
-    useEffect( () => {
-      async function loadDevs() {
-        const response = await api.get('/devs');
-          setDevs(response.data);
-      }
-        loadDevs();
-    }, [] );
+      useEffect( () => {
+        async function loadDevs() {
+          const response = await api.get('/devs');
+            setDevs(response.data);
+        }
+          loadDevs();
+      }, [] );
 
-      async function handleAddDev(e) {
-        e.preventDefault();
-          const response = await api.post('/devs', { github_username, techs, latitude, longitude });
-            setGithubUsername('');
-            setTechs('');
+        async function handleAddDev(data) {
+          const response = await api.post('/devs', data);  
             setDevs([ ...devs, response.data ]);
-      }
+        }
 
-        return (
-          <div id="app">
-            <aside>
-              <div className="brand"> </div>
-                <form onSubmit={ handleAddDev }>
-                  <div className="input-block">
-                    <label htmlFor="github_username"> Usuário do GitHub </label>
-                      <input name="github_username" id="github_username" required value={ github_username } onChange={ e => setGithubUsername(e.target.value) }/>
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="techs"> Tecnologias </label>
-                      <input name="techs" id="techs" required value={ techs } onChange={ e => setTechs(e.target.value) }/>
-                  </div>
-                    <div className="input-group">
-                      <div className="input-block">
-                        <label htmlFor="latitude"> Latitude </label>
-                          <input type="number" name="latitude" id="latitude" required value={ latitude } onChange={ e => setLatitude(e.target.value) }/>
-                      </div>
-                      <div className="input-block">
-                        <label htmlFor="longitude"> Longitude </label>
-                        <input type="number" name="longitude" id="longitude" required value={ longitude } onChange={ e => setLongitude(e.target.value) }/>
-                      </div>
-                    </div>
-                      <button type="submit"> Salvar </button>
-                </form>
-            </aside>
+          return (
+            <div id="app">
+              <aside>
+                <div className="brand"> </div>
+                  <DevForm onSubmit={ handleAddDev } />
+              </aside>
               <main>
                 <ul>
                   { devs.map( dev => (
-                    <li key={ dev._id } className="dev-item">
-                      <header>
-                        <img src={ dev.avatar_url } alt={ dev.name }/>
-                          <div className="user-info">
-                            <strong> { dev.name } </strong>
-                              <span> { dev.techs.join(', ') } </span>
-                          </div>
-                      </header>
-                        <p> { dev.bio } </p>
-                          <a href={ `https://github.com/${ dev.github_username }` }> Acessar Perfil no GitHub </a>
-                    </li>
+                    <DevItem key={ dev._id } dev={ dev }/>
                   ) ) }
                 </ul>
               </main>
-          </div>
-        );
-}
+            </div>
+          );
+  }
 
-  export default App;
+    export default App;
