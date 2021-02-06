dc7af6099684a5ad0fd4af4ef69c964e7914bbb5
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-06 19:34:48 -0300

    Aplicação dos novos arquivos do Back-End.
    Aplicados os novos arquivos (substituindo os antigos bugados) do Back-End que foram refatorados, e tiveram os bugs encontrados durante o desenvolvimento, corrigidos.

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+const { findConnections, sendMessage } = require('../websocket');
+
+    module.exports = {
+            async index (request, response) {
+                const devs = await Dev.find();
+                    return response.json(devs);
+            },
+            async store(request, response) {
+                const { github_username, techs, latitude, longitude } = request.body;
+                    let dev = await Dev.findOne({ github_username });
+                        if(!dev) {
+                            const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+                            const { name = login, bio, avatar_url } = apiResponse.data;   
+                            const techsArray = parseStringAsArray(techs);
+                            const location = { type: 'Point', coordinates: [longitude, latitude] };
+                                dev = await Dev.create({ name, github_username, bio, avatar_url, techs: techsArray, location });
+                            const sendSocketMessageTo = findConnections({ latitude, longitude }, techsArray);
+                                    sendMessage(sendSocketMessageTo, 'new-dev', dev);
+                        }
+                            return response.json(dev);
+            }
+    };
