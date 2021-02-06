4a112b87bf291c3f3686eaf76f16e81897e7c9a7
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-06 20:31:09 -0300

    Aplicação dos novos arquivos do Mobile.
    Aplicados os novos arquivos (substituindo os antigos bugados) do Mobile que foram refatorados, e tiveram os bugs encontrados durante o desenvolvimento, corrigidos.

---
+++ mobile/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StatusBar, LogBox } from 'react-native';
+import Routes from './src/routes';
+
+  LogBox.ignoreLogs([ 'Unrecognized WebSocket' ]);
+
+  export default function App() {
+    return (
+      <>
+        <StatusBar barStyle="light-content" backgroundColor="#7D40E7"/>
+          <Routes/>
+      </>
+    );
+  }
---
+++ mobile/src/services/socket.js
@@ -0,0 +1,20 @@
+import socketio from 'socket.io-client';
+
+    const socket = socketio('http://192.168.1.38:3333', { autoConnect: false });
+
+        function subscribeToNewDevs(subscribeFunction) {
+            socket.on('new-dev', subscribeFunction);
+        }
+
+        function connect(latitude, longitude, techs) {
+            socket.io.opts.query = { latitude, longitude, techs };
+            socket.connect();
+        }
+
+        function disconnect() {
+            if(socket.connected) {
+                socket.disconnect();
+            }
+        }
+
+            export { subscribeToNewDevs, connect, disconnect };
