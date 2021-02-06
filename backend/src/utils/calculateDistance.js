f6c0d4c80a289bed45141248a8db861d1da0c0dd
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-06 19:53:33 -0300

    Ajustes gerais de Indentação.
    Ajustes gerais na Indentação de alguns arquivos do Projeto.

--- backend/src/utils/calculateDistance.js
+++ backend/src/utils/calculateDistance.js
@@ -11,10 +11,7 @@ function deg2rad(deg) {
         const dLat = deg2rad(lat2 - lat1);
         const dLon = deg2rad(lon2 - lon1);
 
-        const a = 
-            Math.sin(dLat/2) * Math.sin(dLat/2) +
-            Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-            Math.sin(dLon/2) * Math.sin(dLon/2);
+        const a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
 
         const center = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
         const distance = radius * center;
