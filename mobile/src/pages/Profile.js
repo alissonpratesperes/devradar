3e02330dfbd7ea67c868839f80efb3eb6969dd71
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-02-06 21:12:08 -0300

    Ajustes gerais de Indentação.
    Ajustes gerais na Indentação de alguns arquivos do Projeto.

--- mobile/src/pages/Profile.js
+++ mobile/src/pages/Profile.js
@@ -4,7 +4,6 @@ import { WebView } from 'react-native-webview';
 
     function Profile({ navigation }) {
         const githubUsername = navigation.getParam('github_username');
-
             return <WebView style={{ flex: 1 }} source={{ uri: `https://github.com/${githubUsername}` }} />
     }
 
--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,20 +1,11 @@
 import socketio from 'socket.io-client';
 
-    const socket = socketio('http://192.168.1.38:3333', { autoConnect: false });
+    const socket = socketio('http://192.168.0.102:3333', { autoConnect: false });
 
-        function subscribeToNewDevs(subscribeFunction) {
-            socket.on('new-dev', subscribeFunction);
-        }
+        function subscribeToNewDevs(subscribeFunction) { socket.on('new-dev', subscribeFunction); }
 
-        function connect(latitude, longitude, techs) {
-            socket.io.opts.query = { latitude, longitude, techs };
-            socket.connect();
-        }
+        function connect(latitude, longitude, techs) { socket.io.opts.query = { latitude, longitude, techs  };  socket.connect(); }
 
-        function disconnect() {
-            if(socket.connected) {
-                socket.disconnect();
-            }
-        }
+        function disconnect() { if(socket.connected) { socket.disconnect(); } }
 
             export { subscribeToNewDevs, connect, disconnect };
