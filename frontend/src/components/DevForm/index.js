4de525cf5f98b784b341d22592384cc5bae2bfac
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-12 13:53:55 -0300

    Correções gerais na Estilização CSS Proprietária no Front-End da Aplicação.
    Realizadas algumas correções gerias na Estilização CSS Proprietária no Front-End da Aplicação.

--- frontend/src/components/DevForm/index.js
+++ frontend/src/components/DevForm/index.js
@@ -38,11 +38,11 @@ import './styles.css';
                             <div className="input-group">
                                 <div className="input-block">
                                     <label htmlFor="latitude"> Latitude </label>
-                                        <input type="number" name-="latitude" id="latitude" required value={latitude} onChange={ e => setLatitude(e.target.value)}/>
+                                        <input type="number" name="latitude" id="latitude" required value={latitude} onChange={ e => setLatitude(e.target.value)}/>
                                 </div>
                                 <div className="input-block">
                                     <label htmlFor="longitude"> Longitude </label>
-                                        <input type="number" name-="longitude" id="longitude" required value={longitude} onChange={ e => setLongitude(e.target.value)}/>
+                                        <input type="number" name="longitude" id="longitude" required value={longitude} onChange={ e => setLongitude(e.target.value)}/>
                                 </div>
                             </div>
                                 <button type="submit"> Cadastrar </button>
