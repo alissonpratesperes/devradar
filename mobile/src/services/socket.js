8a6f80039c16431cbec7dff137ece559feea1544
Alisson Prates Peres <52282116+alissonpratesperes@users.noreply.github.com>
2021-04-17 17:32:27 -0300

    Testes de Build com a Aplicação.
    Realizados testes de Build com os Bundlers para testar a Aplicação devido à organização dos arquivos das Aulas.

--- mobile/src/services/socket.js
+++ mobile/src/services/socket.js
@@ -1,6 +1,6 @@
 import socketio from 'socket.io-client';
 
-    const socket = socketio('http://192.168.1.38:3333', { autoConnect: false });
+    const socket = socketio('http://192.168.0.102:3333', { autoConnect: false });
 
         function subscribeToNewDevs(subscribeFunction) { socket.on('new-dev', subscribeFunction); }
         function connect(latitude, longitude, techs) { socket.io.opts.query = { latitude, longitude, techs  };  socket.connect(); }
